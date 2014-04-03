b146373ec2b2105459985b5c632e4ded67341774
jannik.lorenzen <jannik.lorenzen@innogames.de>
2014-04-03 18:03:45 +0200

    Forex: nothing to see hrere (yet)

--- src/application/classes/forex/Forex.php
+++ src/application/classes/forex/Forex.php
@@ -6,11 +6,7 @@ class Forex
 	const FOREX_JPY = 'JPY';
 	const FOREX_CAD = 'CAD';
 	
-	public function __construct() {
-		
-	}
-	
-	public function getExchange($from, $to) {
+	public static function getExchange($from, $to) {
 		
 	}
 }
