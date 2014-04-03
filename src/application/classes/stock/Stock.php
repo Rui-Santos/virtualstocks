16f899319ea5377e01cad5909e41db9d4ee34911
jannik.lorenzen <jannik.lorenzen@innogames.de>
2014-04-03 17:29:27 +0200

    further basic implementation (Stock-class)

--- src/application/classes/stock/Stock.php
+++ src/application/classes/stock/Stock.php
@@ -6,43 +6,69 @@ class Stock
 	 * Stores the stock symbol (eg. CBK)
 	 * @var String $stockSymbol
 	 */
-	public $stockSymbol;
+	protected $stockSymbol;
 	
 	/**
 	 * Stores the stocks short name (eg. Commerzbank)
 	 * @var String $stockIdentifierName
 	 */
-	public $stockIdentifierName;
+	protected $stockIdentifierName;
 	
 	/**
 	 * Stores the full qualified name
 	 * @var String $stockName
 	 */
-	public $stockName;
+	protected $stockName;
 	
 	/**
 	 * Stores the ISIN
 	 * @var String $stockISIN
 	 */
-	public $stockISIN;
+	protected $stockISIN;
 	
 	/**
 	 * Stores the country-(ISO)code
 	 * @var String $stockCountryISO
 	 */
-	public $stockCountryISO;
+	protected $stockCountryISO;
 	
 	/**
 	 * Stores the stocks categoryId
 	 * @var Integer $stockCategoryId
 	 */
-	public $stockCategoryId;
+	protected $stockCategoryId;
 
-	public function __construct($stockSymbol = null) {
-		if (!is_null($stockSymbol)) {
-			$this->stockSymbol = $stockSymbol;
-			$this->load();
-		}
+	public function __construct($stockSymbol) {
+		$this->stockSymbol = $stockSymbol;
+		$this->load();
+	}
+	
+	public function getSymbol() {
+		return $this->stockSymbol;
+	}
+	
+	public function getIdentifierName() {
+		return $this->stockIdentifierName;
+	}
+	
+	public function getName() {
+		return $this->stockName;
+	}
+	
+	public function getISIN() {
+		return $this->stockISIN;
+	}
+	
+	public function getCountryISO() {
+		return $this->stockCountryISO;
+	}
+	
+	public function getCategoryId() {
+		return $this->stockCategoryId;
+	}
+	
+	public function getLatestRate($exMarket) {
+		return DB::get()->query("SELECT stock_rate FROM vs_stock_rate WHERE stock_symbol = '%s' AND stock_exchange  = '%d' ORDER BY stock_timestamp DESC LIMIT 1", $this->stockSymbol, $exMarket)->fetch_value();
 	}
 	
 	private function load($stockSymbol) {
