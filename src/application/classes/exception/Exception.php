ea40651ec4b71c33952ccbde7662d0bb926c63ed
jl0renzen <j.lorenzen@dioxidsoft.com>
2014-04-04 19:30:08 +0200

    Customized exceptions
    w/o custom behaviour yet

---
+++ src/application/classes/exception/Exception.php
@@ -0,0 +1,7 @@
+<?php
+
+DepotException extends Exception {}
+ExchangeException extends Exception {}
+ForexException extends Exception {}
+DBException extends Exception {}
+StockException extends Exception {}
